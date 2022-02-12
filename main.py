4a0ffb6b789c26f3ecf5a58939e311ee1f071084
rahulkacha <rahulbkacha@gmail.com>
2022-02-12 17:35:10 +0530

    changed postgresDB env var

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
